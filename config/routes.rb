5acf37544d54c4b6d6667a2797ebeb6730b122df
Dexter Maugao <techandcoder@gmail.com>
2022-10-14 22:19:21 +0800

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'home/index'
+  get 'other/index'
+
+  get '/home/turbo_frame_form' => 'home#turbo_frame_form', as: 'turbo_frame_form'
+  post '/home/turbo_frame_submit' => 'home#turbo_frame_submit', as: 'turbo_frame_submit'
+
+
+  root to: "home#index"
+end
+
